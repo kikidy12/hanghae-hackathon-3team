0b99675b181bae2a22eb4b4ce23693ebbba03b5a
음지훈 <jhmc5268@naver.com>
2023-01-17 04:42:31 +0900

    feat: DB수정

--- db.py
+++ db.py
@@ -3,5 +3,5 @@ import certifi
 
 ca = certifi.where()
 client = MongoClient('mongodb+srv://test:sparta@cluster0.2caednd.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.recommendbook
+db = client.dbsparta
 
