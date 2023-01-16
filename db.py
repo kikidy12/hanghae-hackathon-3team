829385bd9a7823798f38fd79e433f492f8d8d6f0
kikidy12 <kikidy12@naver.com>
2023-01-17 04:55:19 +0900

    디비 변경 라이브러리 추가

--- db.py
+++ db.py
@@ -2,6 +2,9 @@ from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.2caednd.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta
+
+client = MongoClient(
+        "mongodb+srv://test:sparta@cluster0.zphghpj.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client.recommendbook
+
 
