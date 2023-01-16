5ccff732c702b4b42cdc2c4bb496bce082904f19
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-01-16 13:48:54 +0900

    이도경_간단한 틀 완성_230116_13:48

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, jsonify, session
+from flask import Flask, render_template, jsonify, session, redirect, url_for
 from user import *
 from bookManage import *
 from book import *
 from comment import *
+# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
+import jwt
+
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
+import datetime
+
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
+import hashlib
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template('index.html', nickname=user_info["nick"])
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
 @app.route('/api/book')
@@ -18,16 +34,29 @@ def apiBookList():
     return test
 
 
-@app.route('/signUp')
+@app.route('/register')
 def signUp():
     return render_template('signUp.html')
 
+@app.route('/logIn')
+def signUp():
+    return render_template('signIn.html')
 
-@app.route('/signUp/giveInfo', methods=["POST"])
+
+@app.route('/api/register', methods=['POST'])
 def giveUserInfo():
     giveUserInfo = apiRegester()
     return giveUserInfo
 
+@app.route('/api/login', methods = ['POST'])
+def apiLogIn():
+    logIn = apiLogin()
+    return logIn
+
+@app.route('/api/nickName', methods = ['GET'])
+def apiValid():
+    checkUp = apiValid()
+    return checkUp
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5500, debug=True)
