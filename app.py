a15d1f4efe6a331bc88d0c66950df81fcfcef56a
kikidy12 <kikidy12@naver.com>
2023-01-16 14:40:13 +0900

    메인화면 책 목록 조회 api 추가 댓글 등록 삭제 목록조회 api 추가 책 상세조회 api 추가

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+
 from flask import Flask, render_template, jsonify, session
 from user import *
 from bookManage import *
-from book import *
-from comment import *
+from book import getBook, getBookList
+from comment import addComment, addSubComment, getCommentList, deleteComment
 
 app = Flask(__name__)
 
@@ -10,11 +11,39 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/api/bookdetail')
+def apiGetBook():
+  json = getBook()
+  return json
 
-@app.route('/api/book')
-def apiBookList():
-  test = getBookList()
-  return test
+@app.route('/api/booklist')
+def apiGetBookList():
+  json = getBookList()
+  return json
+
+@app.route('/api/commentlist', methods=["GET"])
+def apiGetCommentList():
+  json = getCommentList()
+  return json
+
+
+@app.route('/api/comment', methods=["POST"])
+def apiPostComment():
+  message = addComment()
+  return message
+
+
+@app.route('/api/subcomment', methods=["POST"])
+def apiPostSubComment():
+  message = addSubComment()
+  return message
+
+
+
+@app.route('/api/comment', methods=["DELETE"])
+def apiDeleteComment():
+  message = deleteComment()
+  return message
   
 
 if __name__ == '__main__':
--- comment.py
+++ comment.py
@@ -1,2 +1,55 @@
 from db import db
 from flask import jsonify, request
+
+
+# 댓글 목록 조회
+def getCommentList():
+  commentList = list(db.comment.find({}, {'_id': False}))
+  return jsonify({'commentList':commentList})
+
+
+# 댓글 등록
+def addComment():
+  userId = int(request.form['uesrId']);
+  bookId = int(request.form['bookId']);
+  comment = request.form['comment'];
+
+  # 커맨트 id순으로 정렬
+  commentList = list(db.comment.find({}, {'_id': False}).sort('id'))
+
+  # 마지막 번호를 호출한다
+  lastId = commentList[-1]['id'] if len(commentList) > 0 else 1
+
+  comment = db.comment.insert_one({'id': lastId, 'userId': userId, 'bookId': bookId, 'comment': comment, 'isSub': False })
+
+  return jsonify({'message':'등록완료'})
+
+
+# 대댓글 등록
+def addSubComment():
+  userId = int(request.form['uesrId']);
+  bookId = int(request.form['bookId']);
+  commentId = int(request.form['commentId']);
+  comment = request.form['comment'];
+
+  # 장소목록을 num순으로 정렬
+  comment_list = list(db.comment.find({}, {'_id': False}).sort('id'))
+
+  # 마지막 번호를 호출한다
+  lastId = comment_list[-1]['id'] if len(comment_list) > 0 else 1
+
+  comment = db.comment.insert_one({'id': lastId, 'userId': userId, 'bookId': bookId, 'commentId': commentId, 'comment': comment, 'isSub': True })
+
+  return jsonify({'message':'등록완료'})
+
+
+
+# 댓글 삭제 (삭제시 하위에 달려있는 댓글까지 모두 삭제)
+def deleteComment():
+  userId = int(request.form['uesrId']);
+  commentId = int(request.form['commentId']);
+  
+  db.comment.delete_one({"id":commentId, 'userId': userId})
+  db.comment.delete({"commentId":commentId})
+
+  return jsonify({'message':'삭제완료'})
